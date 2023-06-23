5ef59aa785d6c76db1180ea92b9b8ccf157214fd
Bohdan <b.yaronnyi@gmail.com>
2023-06-23 17:15:16 +0300

    security cam project uppdate

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ btn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
