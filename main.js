d4ee9d01ee7eaa5bf4da7eb41d57101ce4af0213
Bohdan <b.yaronnyi@gmail.com>
2023-06-23 15:39:14 +0300

    security cam project  uppdate

---
+++ main.js
@@ -0,0 +1,15 @@
+// Кнопка вверх upTop
+var btn = $("#upTop");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
